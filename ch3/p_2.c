9d201fccf8481a1a35bb6f82cbaea8d546de85d1
Alexander Pobereznichenko <apobereznichenko@gmail.com>
2016-08-24 23:41:03 +0300

    Chapter3: finished

---
+++ ch3/p_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int number, month, day, year;
+   float price;
+   printf("Enter item number: ");
+   scanf("%i", &number);
+   printf("Enter unit price: ");
+   scanf("%f", &price);
+   printf("Enter purchase date (mm/dd/yyy): ");
+   scanf("%d/%d/%d", &month, &day, &year);
+
+   printf("Item\tPrice\t\tDate\n%d\t$%7.2f\t%2d/%2d/%2d\n", number, price, month, day, year);
+
+   return 0;
+}
